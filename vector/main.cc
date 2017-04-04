9663d690f698519e22040afb8d03ff28e5325d03
XiaoyuGuo <16126144@bjtu.edu.cn>
2017-04-05 06:42:45 +0800

    vector

--- vector/main.cc
+++ vector/main.cc
@@ -1,19 +1,54 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "qh_vector.h"
 
+void test_vector()
+{
+	using qh::vector;
+
+	bool all_test_ok = true;
+
+	vector<int> test;
+	test.push_back(1);
+	assert(test[0] == 1);
+	vector<int> test_v(10, 1);
+	assert(test_v.size() == 10);
+
+	for (int i = 0; i < 10; i++) {
+		assert(test_v[i] == 1);
+	}
+
+	test_v.resize(5);
+	assert(test_v.size() == 5);
+
+	test_v.push_back(6);
+	assert(test_v.size() == 6);
+	assert(test_v[5] == 6);
+
+	test_v.pop_back();
+	assert(test_v.size() == 5);
+
+	test_v.clear();
+	assert(test_v.size() == 0);
+	assert(test_v.empty());
+
+	if (all_test_ok)
+	{
+		printf("%s All test OK!\n", __FUNCTION__);
+	}
+	else
+	{
+		printf("%s test failed!\n", __FUNCTION__);
+	}
+}
 
 int main(int argc, char* argv[])
 {
-    //TODO 在这里添加单元测试，越多越好，代码路径覆盖率越全越好
-    //TODO 单元测试写法请参考INIParser那个项目，不要写一堆printf，要用assert进行断言判断。
-
-    qh::vector<int> num_vect;
-    num_vect.push_back(1);
+	test_vector();
 
 #ifdef WIN32
-    system("pause");
+	system("pause");
 #endif
-
 	return 0;
 }
 
--- vector/qh_vector.h
+++ vector/qh_vector.h
@@ -1,64 +1,154 @@
 #ifndef QIHOO_VECTOR_H_
 #define QIHOO_VECTOR_H_
 
+#include <assert.h>
+
 namespace qh
 {
-    template<class T>
-    class vector {
-    public:
-        //ctor
-        vector()
-            : data_(NULL), size_(0)
-        {
-        }
+	template<class T>
+	class vector {
+	public:
+		//ctor
+		vector()
+			: data_(NULL), size_(0), capacity_(0)
+		{
+		}
 
-        explicit vector( size_t n, const T& value = T())
-        {
-            data_ = new T[n];
-            for (size_t i = 0; i < n; i++)
-            {
-                data_[0] = value;
-            }
-        }
+		//Initialize T[n] with value
+		explicit vector(size_t n, const T& value = T())
+		{
+			data_ = new T[n];
+			capacity_ = n;
+			size_ = n;
 
-        vector<T>& operator=(const vector<T>& rhs)
-        {
-            data_ = rhs.data_;
-            size_ = rhs.size_;
-        }
+			for (size_t i = 0; i < n; i++)
+			{
+				data_[i] = value;
+			}
+		}
 
-        //dtor
-        ~vector()
-        {
-            if (data_)
-            {
-                delete data_;
-            }
-        }
+		vector<T>& operator=(const vector<T>& rhs)
+		{
+			// Avoid self
+			if (this != &rhs)
+			{
+				if (data) {
+					delete[] data_;
+				}
 
-        //get
-        size_t size() const
-        {
-            return size_;
-        }
+				size_ = rhs.size();
+				capacity_ = size_;
+				data_ = new T[capacity_];
+				for (size_t i = 0; i < size_; i++)
+				{
+					// Not deep copy
+					data_[i] = rhs.data_[i];
+				}
+			}
+		}
 
-        // set & get
-        T& operator[](size_t index);
+		//dtor
+		~vector()
+		{
+			if (data_)
+			{
+				delete[] data_;
+			}
+		}
 
-        // set
-        void push_back(const T& element);
-        void pop_back(const T& element);
-        void resize();
-        void reserve();
-        void clear();
-        void empty();
+		//get
+		size_t size() const
+		{
+			return size_;
+		}
 
-    private:
-        T*      data_;
-        size_t  size_;
-    };
+		// set & get
+		T& operator[](size_t index);
+
+		// set
+		void push_back(const T& element);
+		void pop_back();
+		void resize(size_t size);
+		void reserve(size_t capacity);
+		void clear();
+		bool empty();
+
+	private:
+		T*      data_;
+		size_t  size_;
+		size_t capacity_;
+	};
+
+	template<class T>
+	T& vector<T>::operator[](size_t index)
+	{
+		assert(index < size_);
+		assert(index >= 0);
+		return data_[index];
+	}
+
+	template<class T>
+	void vector<T>::push_back(const T& element) {
+		if (size_ == capacity_)
+		{
+			reserve(size_ * 2 + 1);
+		}
+		data_[size_] = element;
+		size_++;
+	}
+
+	template<class T>
+	void vector<T>::pop_back()
+	{
+		assert(size_ > 0);
+		size_--;
+	}
+
+	template<class T>
+	void vector<T>::reserve(size_t capacity)
+	{
+		if (capacity <= size_) return;
+
+		T* tmp_data = data_;
+		data_ = new T[capacity];
+
+		for (size_t i = 0; i < size_; ++i)
+		{
+			data_[i] = tmp_data[i];
+		}
+
+		capacity_ = capacity;
+
+		delete[] tmp_data;
+	}
+
+	template<class T>
+	void vector<T>::resize(size_t size)
+	{
+		if (size > size_)
+		{
+			// Plus 1 avoid size == 0
+			reserve(2 * size + 1);
+		}
+		else
+		{
+			size_ = size;
+		}
+
+	}
+
+	template<class T>
+	void vector<T>::clear()
+	{
+		size_ = 0;
+	}
+
+	template<class T>
+	bool vector<T>::empty()
+	{
+		return size_ == 0;
+	}
 }
-
 #endif
 
 
